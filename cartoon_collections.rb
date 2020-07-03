5829b98bbbc537227b25239c1b74ec55a877fc64
Jessica  Rattani <jjrattani@gmail.com>
2020-07-03 03:15:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -38,7 +38,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.each do |good_calls|
     if valid_calls.include? good_calls
-      puts good_calls
+      puts good_calls "Earth"
     end
   end
 end
